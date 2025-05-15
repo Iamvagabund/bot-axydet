92c87b25b5266a2b903604c77f536ece1256485f
yuri <yuri.vynnyk@gmail.com>
2025-05-16 02:34:49 +0300

    add admin fix

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ load_dotenv()
 # Telegram Bot Token
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
-ADMIN_IDS = [int(id) for id in os.getenv('ADMIN_IDS', '').split(',')]
+ADMIN_IDS = [int(id) for id in os.getenv('ADMIN_IDS', '').split(',') if id.strip()]
+
 GROUP_CHAT_ID = int(os.getenv('GROUP_CHAT_ID'))
 
 # Training Types
